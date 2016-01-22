1effa0260278b093cefec0d89892802d877a7f19
makambi <makambi.jb@gmail.com>
2016-01-22 23:45:17 +0200

    Basic decomposition with dummy frequency. Need to find out seasonal trends.

--- TimeSeries.R
+++ TimeSeries.R
@@ -1,15 +1,39 @@
-data_path <- "Desktop/DataAnalysis/Soft/time-series.csv"
-data <- read.csv(data_path, header = TRUE, sep = '\t')
-data_ts <- ts(data)
-plot.ts(data_ts)
-
 #http://a-little-book-of-r-for-time-series.readthedocs.org/en/latest/src/timeseries.html
 #http://www.slideshare.net/rdatamining/time-series-analysis-and-mining-with-r
+
+
+data_path <- "Desktop/DataAnalysis/Soft/time-series.csv"
+data <- read.csv(data_path, header = TRUE, sep = '\t')
+
+dat1 <- data[,c("Dat1")]
+dat2 <-data[,c("Dat2")]
+data <- data[,c("Dat1", "Dat2")]
+
+#find actual number of periods (frequency)
+dat1_ts <- ts(dat1, frequency = 12)
+dat2_ts <- ts(dat2)
+data_ts <- ts(data)
+
+plot.ts(data_ts)
+
+library("TTR")
+dat1_sma25 <- SMA(dat1_ts, 5)
+
+par(mfrow=c(2,1))
+plot.ts(dat1_sma25)
+plot.ts(dat1_ts)
+
+#also check stl for decomposing
+decompose(dat1_ts)
+
 
 f <- decompose(data_ts[,c('N', 'Dat1')])
 
 library("TTR")
 dat1_sma <- SMA(data_ts[,'Dat1'],n=10)
+
+
+
 plot.ts(dat1_sma)
 
 decompose(dat1_sma)
