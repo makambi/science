2b791e385f29cda5a7fbb2c6ffcbec22fe9d43a0
makambi <makambi.jb@gmail.com>
2016-01-24 17:11:13 +0200

    Added basic ARIMA model code, links for checking stationary and seasonality of data

--- TimeSeries.R
+++ TimeSeries.R
@@ -8,6 +8,10 @@
 
 #http://stackoverflow.com/questions/10302261/forecasting-time-series-data
 
+#Forecasting: principles and practice » ARIMA models
+#https://www.otexts.org/fpp/8/1  
+
+
 data_path <- "Desktop/DataAnalysis/Soft/time-series.csv"
 data <- read.csv(data_path, header = TRUE, sep = '\t')
 
@@ -16,7 +20,7 @@ dat2 <-data[,c("Dat2")]
 data <- data[,c("Dat1", "Dat2")]
 
 #find actual number of periods (frequency)
-dat1_ts <- ts(dat1, frequency = 12)
+dat1_ts <- ts(dat1) #, frequency = 12)
 dat2_ts <- ts(dat2)
 data_ts <- ts(data)
 
@@ -47,10 +51,29 @@ plot.ts(components_adjusted)
 plot.ts(dat1_ts)
 
 
+library(stats)
+
+#the ACF plot is also useful for identifying non-stationary time series
+
+#For a stationary time series, the ACF will drop to zero relatively quickly
+acf(dat2_ts)
+
+#he ACF of non-stationary data decreases slowly
+acf(dat1_ts)
 
 
+#http://www.r-bloggers.com/unit-root-tests/
+library(tseries)
 
+#http://stats.stackexchange.com/questions/27332/how-to-know-if-a-time-series-is-stationary-or-non-stationary
 
+# The null-hypothesis for an ADF test is that the data are non-stationary. So large p-values are indicative of non-stationarity, 
+# and small p-values suggest stationarity. Using the usual 5% threshold, differencing is required if the p-value is greater than 0.05.
+adf.test(dat1_ts, alternative = "stationary")
+adf.test(dat2_ts, alternative = "stationary")
+
+kpss.test(dat1_ts)
+kpss.test(dat2_ts)
 
 x <- xts(x=data$Dat1, order.by = as.Date(seq(1, nrow(data))))
 x.ts = ts(x, freq=365)
